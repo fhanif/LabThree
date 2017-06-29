4884a1444ac559272dfada949d33dbf6400f6386
Farha Hanif <farhahanif94@gmail.com>
2017-06-29 10:24:07 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -18,39 +18,41 @@ public class Main {
 
             userNum = scnr.nextInt();
 
-            /*
-            System.out.println("Enter an integer: ");
-            userNum = scnr.nextInt();
-
-            while (!scnr.hasNextInt()) {
-                System.out.println("Invalid entry. Please enter an integer value.");
-                scnr.nextLine();
-                userNum = scnr.nextInt();
-            }*/
-            //WHY DOES THIS NOT WORK???
-
-           //Formatting columns - padding
+            //Formatting columns - padding
             System.out.printf("%-22s%-22s%-22s\n", "Number", "Squared", "Cubed");
             System.out.printf("%-22s%-22s%-22s\n", "======", "=======", "=====");
 
 
             int i = 0;
-            for (i = 1; i <= Math.abs(userNum); i++) { //for loop for finding squares and cubes
-                int iSquared;
-                int iCubed;
-                iSquared = (int) Math.pow(i, 2); //abs seems to make no diff -- still doesn't work for neg #
-                iCubed = (int) Math.pow(i, 3);
-                System.out.printf("%-22d%-22d%-22d\n", i, iSquared, iCubed); //printing answers in columns
+            if (userNum > 0) {
+                for (i = 1; i <= Math.abs(userNum); i++) { //for loop for finding squares and cubes
+                    int iSquared;
+                    int iCubed;
+                    iSquared = (int) Math.pow(i, 2);
+                    iCubed = (int) Math.pow(i, 3);
+                    System.out.printf("%-22d%-22d%-22d\n", i, iSquared, iCubed); //printing answers in columns
+                }
+            }
+            else if (userNum < 0) {
+                for (i = -1; i >= userNum; i--) { //for loop for finding squares and cubes
+                    int iSquared;
+                    int iCubed;
+                    iSquared = (int) Math.pow(i, 2);
+                    iCubed = (int) Math.pow(i, 3);
+                    System.out.printf("%-22d%-22d%-22d\n", i, iSquared, iCubed); //printing answers in columns
+                }
             }
 
-            scnr.nextLine();
-            System.out.println("Would you like to continue? (Y/N)");
-            keepGoing = scnr.nextLine();
+
+
+        scnr.nextLine();
+        System.out.println("Would you like to continue? (Y/N)");
+        keepGoing = scnr.nextLine();
         //validation for continuing -- do while loop
-        } while (keepGoing.equalsIgnoreCase("y") && (!keepGoing.equalsIgnoreCase("N")));
+    } while(keepGoing.equalsIgnoreCase("y")&&(!keepGoing.equalsIgnoreCase("N")));
 
         System.out.println("End of program.");
-    }
-//why won't it work for negative numbers???
+}
+//finally works, not sure about the formatting lines on the for loops 
 }
 
